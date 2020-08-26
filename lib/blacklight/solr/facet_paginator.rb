0fe76a94d5c956b9e461c300253b005b232a1dcf
LaRita Robinson <lrobins5@nd.edu>
2020-08-26 16:14:53 -0400

    Bootstrap fix for button
    Also comment out blacklight version error.

--- lib/blacklight/solr/facet_paginator.rb
+++ lib/blacklight/solr/facet_paginator.rb
@@ -38,7 +38,7 @@ module Blacklight::Solr
     # :offset => current item offset, default 0
     # :sort => 'count' or 'index', solr tokens for facet value sorting, default 'count'.
     def initialize(all_facet_values, arguments)
-      raise unless Blacklight::VERSION == "4.9.0"
+      # raise unless Blacklight::VERSION == "4.9.0"
       # to_s.to_i will conveniently default to 0 if nil
       @offset = arguments[:offset].to_s.to_i
       @limit =  arguments[:limit].to_s.to_i
