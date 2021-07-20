4423b85c4e6a6a1b55d7749a2ae2b7a70f6ae221
Mark Suhovecky <suhovecky@nd.edu>
2021-07-20 14:47:41 -0400

    first pass of alt thumbail

--- lib/sufia/models/generic_file/thumbnail.rb
+++ lib/sufia/models/generic_file/thumbnail.rb
@@ -1,3 +1,5 @@
+require 'pdf_thumbnailer'
+
 module Sufia
   module GenericFile
     module Thumbnail
@@ -7,9 +9,14 @@ module Sufia
       def create_thumbnail
         return unless self.content.has_content?
 
+        logger.info "CURATE228 call create_thumbnail"
         if video?
           create_video_thumbnail
+        elsif pdf?
+        logger.info "CURATE228 PDF thumbnail"
+          create_pdf_thumbnail
         else
+        logger.info "CURATE228 derivative thumbnail"
           create_derivatives
         end
         self.save
@@ -30,6 +37,21 @@ module Sufia
 
         self.thumbnail.content = File.open(output_file, 'rb').read
         self.thumbnail.mimeType = 'image/png'
+      end
+
+      def create_pdf_thumbnail
+
+        PdfThumbs.configure img_dir: '/tmp', thumb_sizes: 493
+
+        output_file = Dir::Tmpname.create(['PDFThumbnailer', ".png"], '/tmp'){}
+
+        content.to_tempfile do |f|
+          num_pages = PdfThumbs.thumbnail_single! "/tmp", File.basename(f.path)
+        end
+
+        self.thumbnail.content = File.open(output_file, 'rb').read
+        self.thumbnail.mimeType = 'image/png'
+        self.save
       end
 
     end
