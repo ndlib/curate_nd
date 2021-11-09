fe4852c868d68147ece197249d085d6c7a45b8a5
Mark Suhovecky <suhovecky@nd.edu>
2021-11-09 09:54:35 -0500

    Update file_content_datastream.rb

--- app/repository_datastreams/file_content_datastream.rb
+++ app/repository_datastreams/file_content_datastream.rb
@@ -10,13 +10,21 @@ class FileContentDatastream < ActiveFedora::Datastream
   def extract_metadata
     return unless has_content?
 
-    # I want to run Clam first, let that possibly raise exceptions
-    # Then run fits and return that
+    
+    # if the content is in bendo, file_content will be an open file descriptor to the downloaded file,
+    # if not, it will be the ActiveFedora object's content datastream
     file_content = determine_filesource
+
+    # Run Clam first, let that possibly raise exceptions
+    # Then run fits and return that
     _clam, fits = Hydra::FileCharacterization.characterize(file_content, filename_for_characterization.join(''), :clam,
                                                            :fits) do |config|
       config[:clam] = antivirus_runner
       config[:fits] = characterization_runner
+    end
+    # If we downloaded it ourselves, delete the tempfile
+    if file_content.is_a?(File)
+      File.delete(file_content.path)
     end
     fits
   end
@@ -36,14 +44,25 @@ class FileContentDatastream < ActiveFedora::Datastream
   end
 
   def determine_filesource
-    if controlGroup == 'R'
+    # For bendo datastreams, download the file directly rather than use the Activerfedora content,
+    # lest for big files we run out of memory.
+    if controlGroup == 'R'  # file is in bendo
+      # Create full download path of file
       download_file_dest = File.join(Figaro.env.curate_worker_tmpdir, File.basename(dsLocation))
+
+      # Set TMPDIR in environment so that open_uri creates its tempfiles in the download area
+      # rather than the system /tmp
+      ENV['TMPDIR'] = Figaro.env.curate_worker_tmpdir + '/tmp'
+
+      # try download from bendo API using dsLocation uri in the datastream
       Down::NetHttp.download(dsLocation, headers: { 'X-Api-Key' => Figaro.env.bendo_api_key },
                                          destination: download_file_dest,
                                          open_timeout: 300,
                                          read_timeout: 3600)
+
+      #Return open file descriptor so Hydra::FileCharacterization knows not to use the content DS
       File.open(download_file_dest)
-    else
+    else # file is in content datastream
       content
     end
   end
